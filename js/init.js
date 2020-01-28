eb0931d5a72a4aae96679c3ef349d8485699cd71
JuanManuelFranco <juanm.francoh@utadeo.edu.co>
2020-01-28 05:56:03 -0500

    enero 28
    copy de inicio, seccion de bachelor,

--- js/init.js
+++ js/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Apps/Web", "Videogames", "VR/AR"],
+			strings: ["User Experience", "Business", "Tech"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
