3b639b0b93029a43d151c67fe1230ea9ab62133a
JuanManuelFranco <juanm.francoh@utadeo.edu.co>
2021-04-14 11:52:35 -0500

    cambio en home presentation

--- js/init.js
+++ js/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["User Experience", "Product Design", "Digital Business"],
+			strings: ["Web/mobile apps", "Interactive Systems", "Games, AR & VR"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 3e3
